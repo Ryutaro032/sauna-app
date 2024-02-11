32fab5c349aae912ebf5ee2a9c08e224b5c1d877
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2024-02-11 20:04:49 +0900

    rubocopに従い修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,13 +92,13 @@ RSpec.configure do |config|
   # Kernel.srand config.seed
 
   require 'webmock/rspec'
-  require "vcr"
+  require 'vcr'
 
   VCR.configure do |c|
-    c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-    c.hook_into :webmock 
+    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+    c.hook_into :webmock
     c.configure_rspec_metadata!
-    c.filter_sensitive_data("<API_KEY>") { ENV["GOOGLE_API_KEY"] }
+    c.filter_sensitive_data('<API_KEY>') { ENV.fetch('GOOGLE_API_KEY', nil) }
   end
-# ...
+  # ...
 end
