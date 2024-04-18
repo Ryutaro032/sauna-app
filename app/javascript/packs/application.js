859c5e9cc9e709b1b23ec336f72e614b3dba1f2f
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2024-04-18 16:49:34 +0900

    プロフィール・deviseのスマホ用レスポンシブデザインの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
@@ -15,7 +15,7 @@ import './search';
 import ('jquery')
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 
-// window.$ = jQuery;
+window.$ = jQuery;
