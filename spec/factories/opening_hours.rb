15655d8e25a64baf235a7a0b30cc6f0a58556b64
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2024-06-29 17:37:59 +0900

    rubocopに従いコードを修正

--- spec/factories/opening_hours.rb
+++ spec/factories/opening_hours.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :opening_hour do
     facility
     day_of_week { Faker::Number.between(from: 0, to: 6) }
-    opening_time { Time.zone.parse('09:00') } 
+    opening_time { Time.zone.parse('09:00') }
     closing_time { Time.zone.parse('17:00') }
     holiday { false }
   end
