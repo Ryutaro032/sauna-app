4b7991241cbe5c20b26bc19c59403101e5d13864
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2024-07-12 18:12:13 +0900

    投稿の文字指定の追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ApplicationRecord
 
   validates :title, presence: { message: I18n.t('activerecord.errors.models.post.attributes.title.blank') },
                     length: { maximum: 40, message: I18n.t('activerecord.errors.models.post.attributes.title.too_long') }
-  validates :review, length: { maximum: 400, message: I18n.t('activerecord.errors.models.post.attributes.review.too_long') }
+  validates :review, length: { minimum: 20, maximum: 400, message: I18n.t('activerecord.errors.models.post.attributes.review.too_long') }
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', :js, type: :system do
   let(:user) { create(:user) }
-  let(:post) { create(:post, user: user) }
+  let(:facility) { create(:facility) }
+  let(:post) { build(:post, user: user, facility: facility) }
 
   before do
     sign_in user
@@ -10,7 +11,7 @@ RSpec.describe 'Posts', :js, type: :system do
 
   context '投稿が成功する場合' do
     it 'ログインユーザーの投稿ができること' do
-      visit new_post_path
+      visit new_post_path(facility_id: facility.id)
 
       expect(page).to have_content('レビューを投稿する')
 
@@ -23,13 +24,12 @@ RSpec.describe 'Posts', :js, type: :system do
       expect(page).to have_content('レビューを投稿しました')
       expect(Post.last.title).to eq(post.title)
       expect(Post.last.review).to eq(post.review)
-      expect(Post.last.name).to eq(post.name)
     end
   end
 
   context '投稿が失敗する場合' do
     it 'タイトルが40文字以上の時、エラーメッセージが出ること' do
-      visit new_post_path
+      visit new_post_path(facility_id: facility.id)
 
       fill_in 'タイトル', with: 'a' * 41
       fill_in 'レビュー内容', with: post.review
@@ -40,7 +40,7 @@ RSpec.describe 'Posts', :js, type: :system do
     end
 
     it 'タイトルがない時、エラーメッセージが出ること' do
-      visit new_post_path
+      visit new_post_path(facility_id: facility.id)
 
       fill_in 'タイトル', with: ''
       fill_in 'レビュー内容', with: post.review
@@ -50,15 +50,22 @@ RSpec.describe 'Posts', :js, type: :system do
       expect(page).to have_content('タイトルを入力してください')
     end
 
-    it 'レビュー内容が401文字以上の時、エラーメッセージが出ること' do
-      visit new_post_path
+    it 'レビュー内容が20文字未満もしくは401文字以上の時、エラーメッセージが出ること' do
+      visit new_post_path(facility_id: facility.id)
+
+      fill_in 'タイトル', with: post.title
+      fill_in 'レビュー内容', with: 'a' * 19
+
+      click_on '投稿する'
+
+      expect(page).to have_content('レビュー内容は20文字以上400文字以内で入力してください')
 
       fill_in 'タイトル', with: post.title
       fill_in 'レビュー内容', with: 'a' * 401
 
       click_on '投稿する'
 
-      expect(page).to have_content('レビュー内容は400文字以内で入力してください')
+      expect(page).to have_content('レビュー内容は20文字以上400文字以内で入力してください')
     end
   end
 end
