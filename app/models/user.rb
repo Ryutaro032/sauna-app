0a9f8646ade93d02c1360cb5e966cd43011820c6
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2024-07-14 22:21:17 +0900

    rubocopに従い修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
       user.password = SecureRandom.urlsafe_base64
       user.name = 'ゲスト'
       user.id = 1
-      user.icon_image.attach(io: File.open(Rails.root.join('app/assets/images/guest_icon.png')), filename: 'guest_icon.png') 
+      user.icon_image.attach(io: Rails.root.join('app/assets/images/guest_icon.png').open, filename: 'guest_icon.png')
     end
   end
 end
